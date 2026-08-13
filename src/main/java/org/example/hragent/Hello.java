32c21858146c0b0b653f283b8252e7c2123ae0d5
Administrator <admin@example.com>
2026-08-13 21:07:39 +0800

    测试

---
+++ src/main/java/org/example/hragent/Hello.java
@@ -0,0 +1,5 @@
+package org.example.hragent;
+
+public class Hello {
+    private String name;
+}
---
+++ src/test/java/org/example/hragent/HrAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hragent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
