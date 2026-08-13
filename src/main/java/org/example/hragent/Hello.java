c9afec22c0982193f52d2adeee34ed75dbbf9c92
Administrator <admin@example.com>
2026-08-13 22:43:30 +0800

    修改hello

--- src/main/java/org/example/hragent/Hello.java
+++ src/main/java/org/example/hragent/Hello.java
@@ -2,4 +2,12 @@ package org.example.hragent;
 
 public class Hello {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
